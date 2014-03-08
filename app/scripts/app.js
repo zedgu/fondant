eb852737b4aa76b624c1c0ad3b9fc04feee3fab7
zedgu <heartcpu@gmail.com>
2014-03-08 18:05:15 +0800

    将rainbow改成angular-highlight

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('siteApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'hljs'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
--- app/scripts/directives/fdcode.js
+++ app/scripts/directives/fdcode.js
@@ -3,14 +3,10 @@
 angular.module('siteApp')
   .directive('fdCode', function () {
     return {
-      template: '<pre></pre>',
       restrict: 'A',
-      link: function postLink(scope, element, attrs) {
+      link: function postLink(scope, element) {
         var html = element[0].previousElementSibling.innerHTML;
-        Rainbow.color(html.replace(new RegExp(html.match(/( +)</)[1], 'g'), ''), attrs.fdCode, function(code) {
-          console.log(code);
-          element.children().append(code);
-        });
+        scope.code = html.replace(new RegExp(html.match(/( +)</)[1], 'g'), '');
       }
     };
   });
