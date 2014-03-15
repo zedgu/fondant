c931558f2f5140d0aa9444318f940c92bea7297a
zedgu <heartcpu@gmail.com>
2014-03-15 20:50:34 +0800

    增加reset和text样式，修正fdcode

--- app/scripts/directives/fdcode.js
+++ app/scripts/directives/fdcode.js
@@ -7,7 +7,7 @@ angular.module('siteApp')
       scope: true,
       controller: function($scope, $element){
         var html = $element[0].previousElementSibling.innerHTML;
-        $scope.code = html.replace(new RegExp('\n'+html.match(/( +)</)[1], 'g'), '\n');
+        $scope.code = html.replace(new RegExp('\n'+html.match(/( +)/)[1], 'g'), '\n');
       }
     };
   });
